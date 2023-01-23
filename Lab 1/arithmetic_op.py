6b4c26485e342ec05e6fb716931477472f0d3739
Pratik Thombare <thombareps20.extc@coep.ac.in>
2023-01-23 13:15:13 +0530

    initial commit

---
+++ Lab 1/arithmetic_op.py
@@ -0,0 +1,13 @@
+
+# Date = 09-01
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the secound number: "))
+
+# op = input("Enter:- + : addition, - : substraction, * : multiplication, / : division, % : mod ")
+
+print("Addition of {} and {} is: " .format(a, b), a+b)
+print("Substraction of {} and {} is: " .format(a, b), a-b)
+print("Multiplication of {} and {} is: " .format(a, b), a*b)
+print("Division of {} and {} is: " .format(a, b), a/b)
+print("Mod of {} and {} is: " .format(a, b), a % b)
---
+++ Lab 1/loop.py
@@ -0,0 +1,20 @@
+# For loop
+
+a = int(input("Enter the number: "))
+
+addition = 0
+
+for j in range(a + 1):
+    addition = addition + j
+
+print(addition)
+
+
+# While loop
+k = 1
+addition1 = 0
+while (k <= a):
+    addition1 = addition1 + k
+    k = k + 1
+
+print(addition1)
