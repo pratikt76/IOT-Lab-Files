a2a5c40c64b323eeef6ae61479dddf4bd6b4d9d1
Pratik Thombare <thombareps20.extc@coep.ac.in>
2023-01-30 13:29:26 +0530

    changes

--- Lab 3/rfid/rfid_writing.ino
+++ Lab 3/rfid/rfid_writing.ino
@@ -4,20 +4,20 @@
 /*Using Hardware SPI of Arduino */
 /*MOSI (11), MISO (12) and SCK (13) are fixed */
 /*You can configure SS and RST Pins*/
-#define SS_PIN 10  /* Slave Select Pin */
-#define RST_PIN 7  /* Reset Pin */
+#define SS_PIN 10 /* Slave Select Pin */
+#define RST_PIN 7 /* Reset Pin */
 
 /* Create an instance of MFRC522 */
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 /* Create an instance of MIFARE_Key */
-MFRC522::MIFARE_Key key;          
+MFRC522::MIFARE_Key key;
 
 /* Set the block to which we want to write data */
 /* Be aware of Sector Trailer Blocks */
-int blockNum = 2;  
+int blockNum = 2;
 /* Create an array of 16 Bytes and fill it with data */
 /* This is the actual data which is going to be written into the card */
-byte blockData [16] = {"Siddhesh-Thakare"};
+byte blockData[16] = {"Pratik-Thombare"};
 
 /* Create another array to read data from Block */
 /* Legthn of buffer should be 2 Bytes more than the size of Block (16 Bytes) */
@@ -26,7 +26,7 @@ byte readBlockData[18];
 
 MFRC522::StatusCode status;
 
-void setup() 
+void setup()
 {
   /* Initialize serial communications with the PC */
   Serial.begin(9600);
@@ -47,13 +47,13 @@ void loop()
   }
   /* Look for new cards */
   /* Reset the loop if no new card is present on RC522 Reader */
-  if ( ! mfrc522.PICC_IsNewCardPresent())
+  if (!mfrc522.PICC_IsNewCardPresent())
   {
     return;
   }
-  
+
   /* Select one of the cards */
-  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  if (!mfrc522.PICC_ReadCardSerial())
   {
     return;
   }
@@ -71,34 +71,32 @@ void loop()
   Serial.print(F("PICC type: "));
   MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
   Serial.println(mfrc522.PICC_GetTypeName(piccType));
-         
-   /* Call 'WriteDataToBlock' function, which will write data to the block */
-   Serial.print("\n");
-   Serial.println("Writing to Data Block...");
-   WriteDataToBlock(blockNum, blockData);
-   
-   /* Read data from the same block */
-   Serial.print("\n");
-   Serial.println("Reading from Data Block...");
-   ReadDataFromBlock(blockNum, readBlockData);
-   /* If you want to print the full memory dump, uncomment the next line */
-   //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
-   
-   /* Print the data read from block */
-   Serial.print("\n");
-   Serial.print("Data in Block:");
-   Serial.print(blockNum);
-   Serial.print(" --> ");
-   for (int j=0 ; j<16 ; j++)
-   {
-     Serial.write(readBlockData[j]);
-   }
-   Serial.print("\n");
+
+  /* Call 'WriteDataToBlock' function, which will write data to the block */
+  Serial.print("\n");
+  Serial.println("Writing to Data Block...");
+  WriteDataToBlock(blockNum, blockData);
+
+  /* Read data from the same block */
+  Serial.print("\n");
+  Serial.println("Reading from Data Block...");
+  ReadDataFromBlock(blockNum, readBlockData);
+  /* If you want to print the full memory dump, uncomment the next line */
+  // mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+
+  /* Print the data read from block */
+  Serial.print("\n");
+  Serial.print("Data in Block:");
+  Serial.print(blockNum);
+  Serial.print(" --> ");
+  for (int j = 0; j < 16; j++)
+  {
+    Serial.write(readBlockData[j]);
+  }
+  Serial.print("\n");
 }
 
-
-
-void WriteDataToBlock(int blockNum, byte blockData[]) 
+void WriteDataToBlock(int blockNum, byte blockData[])
 {
   /* Authenticating the desired data block for write access using Key A */
   status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockNum, &key, &(mfrc522.uid));
@@ -113,7 +111,6 @@ void WriteDataToBlock(int blockNum, byte blockData[])
     Serial.println("Authentication success");
   }
 
-  
   /* Write data to the block */
   status = mfrc522.MIFARE_Write(blockNum, blockData, 16);
   if (status != MFRC522::STATUS_OK)
@@ -126,19 +123,18 @@ void WriteDataToBlock(int blockNum, byte blockData[])
   {
     Serial.println("Data was written into Block successfully");
   }
-  
 }
 
-void ReadDataFromBlock(int blockNum, byte readBlockData[]) 
+void ReadDataFromBlock(int blockNum, byte readBlockData[])
 {
   /* Authenticating the desired data block for Read access using Key A */
   byte status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockNum, &key, &(mfrc522.uid));
 
   if (status != MFRC522::STATUS_OK)
   {
-     Serial.print("Authentication failed for Read: ");
-     Serial.println(mfrc522.GetStatusCodeName(status));
-     return;
+    Serial.print("Authentication failed for Read: ");
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
   }
   else
   {
@@ -155,7 +151,6 @@ void ReadDataFromBlock(int blockNum, byte readBlockData[])
   }
   else
   {
-    Serial.println("Block was read successfully");  
+    Serial.println("Block was read successfully");
   }
-  
 }
